5e8ed8d760a9656253fd37c7e880e265c54b4093
Schutz <gabrieltempesta@alunos.utfpr.edu.br>
2023-04-06 13:53:01 -0300

    v0.1.1

--- src/pages/DashBoardMaquinas/index.jsx
+++ src/pages/DashBoardMaquinas/index.jsx
@@ -6,15 +6,19 @@ import { useLocation } from 'react-router-dom';
 
 function DashboardMaquinas() {
   const location = useLocation();
-  const nome = location.state.nome;
-  const email = location.state.email;
+  const nameMaquina = location.state.nameMaquina;
+  const nameOperadores = location.state.nameOperadores;
+  const codigoMaquina = location.state.codigoMaquina;
+  const iotMaquina = location.state.iotMaquina;
+
+
   return (
     <div>
       <Menu />
       <div className="DashBoardMaquinas-section">
         <h1>DashboardMaquinas</h1>
         <Card />
-        <p>Teste to the DashboardMaquinas page.</p>
+        <p>Teste to the  DashboardMaquinas page.</p>
       </div>
     </div>
   )
